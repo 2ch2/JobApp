ed6372f725f2cac60bd8ebd54a638528b75843c6
faizanayubi <indianayubi@gmail.com>
2015-08-12 13:11:33 +0530

    Admin Controller added

---
+++ application/controllers/admin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Description of admin
+ *
+ * @author Faizan Ayubi
+ */
+use Shared\Controller as Controller;
+use Framework\RequestMethods as RequestMethods;
+use Framework\Registry as Registry;
+
+class Admin extends Controller {
+    
+    protected function sync($model) {
+        $this->noview();
+        $db = Framework\Registry::get("database");
+        $db->sync(new $model);
+    }
+}
